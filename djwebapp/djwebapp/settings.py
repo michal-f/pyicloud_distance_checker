4368a6b0567bad53f0fdea5009deaac8743818ff
Michał Frąckowiak <michal.frackowiak@stermedia.pl>
2016-08-27 19:43:28 +0200

    urls fix

--- djwebapp/djwebapp/settings.py
+++ djwebapp/djwebapp/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
